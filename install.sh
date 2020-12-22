cde799ff7b1207f5ca1b4f2693d46b3ee78abb5a
Matthew Goldsmith <40172267+matthewwg4@users.noreply.github.com>
2020-12-22 18:03:11 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# install python 3
-brew install python3
+# # install python 3
+brew install python@3.7
 
-# install ffmpeg (dependency of python library pydub)
+# # install ffmpeg
 brew install ffmpeg --with-libvorbis --with-sdl2 --with-theora
 
 # install python libraries
 pip3 install numpy
-pip3 install librosa
-pip3 install vlc
+pip3 install python-vlc
 pip3 install pydub
-pip3 install serial
+pip3 install librosa
+pip3 install pyserial
 pip3 install spotdl
 
 # rewrite player.c with appropriate path
